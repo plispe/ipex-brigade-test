2b357b5313d8a9889b5e59a653522f81d3126a29
Petr Pliska <petr.pliska@post.cz>
2018-10-31 20:26:54 +0100

    Add remove dbs script

--- brigade.js
+++ brigade.js
@@ -2,15 +2,16 @@ const { events, Job } = require("brigadier")
 
 events.on("exec", (brigadeEvent, project) => {
   const dbImage = 'petrpliska/db-dump:0.0.7'
-  // const importDb = new Job('import-db', dbImage)
+  const dbPrefix = 'VPC-2355_'
+  const importDb = new Job('import-db', dbImage)
 
-  // importDb.env['MYSQL_PASSWORD'] = 'NldvYfFTTH'
-  // importDb.env['DB_PREFIX'] = 'VPC-2355_'
-  // importDb.run()
+  importDb.env['MYSQL_PASSWORD'] = 'NldvYfFTTH'
+  importDb.env['DB_PREFIX'] = dbPrefix
+  importDb.run()
 
-  const removeDBs = new Job('remove-dbs', dbImage)
-  removeDBs.tasks = ['./remove_databases.sh']
-  removeDBs.env['MYSQL_PASSWORD'] = 'NldvYfFTTH'
-  removeDBs.env['DB_PREFIX'] = 'VPC-2355_'
-  removeDBs.run()
+  // const removeDBs = new Job('remove-dbs', dbImage)
+  // removeDBs.tasks = ['./remove_databases.sh']
+  // removeDBs.env['MYSQL_PASSWORD'] = 'NldvYfFTTH'
+  // removeDBs.env['DB_PREFIX'] = dbPrefix
+  // removeDBs.run()
 })
