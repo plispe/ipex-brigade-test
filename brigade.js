3dd75215f8b9391c66f9cd72d8bf5b65fcfbe57c
Petr Pliska <petr.pliska@post.cz>
2018-10-31 22:15:42 +0100

    Debug

--- brigade.js
+++ brigade.js
@@ -2,19 +2,23 @@ const { events, Job } = require("brigadier")
 const dbImage = 'petrpliska/db-dump:0.0.9'
 const dbPrefix = 'VPC-2355_'
 
-events.on('create-dbs', () => {
-  const createDBs = new Job('Import mysql databases', dbImage)
-
-  createDBs.env['MYSQL_PASSWORD'] = 'NldvYfFTTH'
-  createDBs.env['DB_PREFIX'] = dbPrefix
-  createDBs.run()
-
+events.on('exec', (e, p) => {
+  console.log(e, p)
 })
 
-events.on('drop-dbs', () => {
-  const dropDBs = new Job('Drop mysql databases', dbImage)
-  dropDBs.tasks = ['./remove_databases.sh']
-  dropDBs.env['MYSQL_PASSWORD'] = 'NldvYfFTTH'
-  dropDBs.env['DB_PREFIX'] = dbPrefix
-  dropDBs.run()
-})
+// events.on('create-dbs', () => {
+//   const createDBs = new Job('Import mysql databases', dbImage)
+
+//   createDBs.env['MYSQL_PASSWORD'] = 'NldvYfFTTH'
+//   createDBs.env['DB_PREFIX'] = dbPrefix
+//   createDBs.run()
+
+// })
+
+// events.on('drop-dbs', () => {
+//   const dropDBs = new Job('Drop mysql databases', dbImage)
+//   dropDBs.tasks = ['./remove_databases.sh']
+//   dropDBs.env['MYSQL_PASSWORD'] = 'NldvYfFTTH'
+//   dropDBs.env['DB_PREFIX'] = dbPrefix
+//   dropDBs.run()
+// })
