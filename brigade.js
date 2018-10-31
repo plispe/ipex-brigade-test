ce5bc56617784ef1ef4483ac190c804a7964bf98
Petr Pliska <petr.pliska@post.cz>
2018-10-31 19:08:45 +0100

    Add db prefix

--- brigade.js
+++ brigade.js
@@ -1,8 +1,9 @@
 const { events, Job } = require("brigadier")
 
 events.on("exec", (brigadeEvent, project) => {
-  const importDb = new Job('import-db', 'petrpliska/db-dump:0.0.5')
+  const importDb = new Job('import-db', 'petrpliska/db-dump:0.0.6')
 
   importDb.env['MYSQL_PASSWORD'] = 'NldvYfFTTH'
+  importDb.env['DB_PREFIX'] = 'VPC-2355_'
   importDb.run()
 })
