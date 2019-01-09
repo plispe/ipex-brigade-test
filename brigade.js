5290846b4144578d2ad01de25adc20ec0bd01830
Petr Pliska <petr.pliska@post.cz>
2019-01-09 23:08:45 +0100

    Change user

--- brigade.js
+++ brigade.js
@@ -5,7 +5,7 @@ const dbPrefix = 'VPC-2355_'
 events.on('create-mysql-dbs', () => {
   const createDBs = new Job('create-mysql-dbs', dbImage)
   createDBs.env['MYSQL_PASSWORD'] = 'ipex-mysql'
-  createDBs.env['MYSQL_USER'] = 'ipex-mysql'
+  createDBs.env['MYSQL_USER'] = 'root'
   createDBs.env['MYSQL_HOST'] = 'mysql.ipex.cloud'
   createDBs.env['DB_PREFIX'] = dbPrefix
   createDBs.run()
@@ -16,7 +16,7 @@ events.on('drop-mysql-dbs', () => {
   const dropDBs = new Job('drop-mysql-dbs', dbImage)
   dropDBs.tasks = ['./remove_databases.sh']
   dropDBs.env['MYSQL_PASSWORD'] = 'ipex-mysql'
-  dropDBs.env['MYSQL_USER'] = 'ipex-mysql'
+  dropDBs.env['MYSQL_USER'] = 'root'
   dropDBs.env['MYSQL_HOST'] = 'mysql.ipex.cloud'
   dropDBs.env['DB_PREFIX'] = dbPrefix
   dropDBs.run()
